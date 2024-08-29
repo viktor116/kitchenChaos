e9da8bc229e5f194998d374dba6bae38fe16edb2
soybean <984149293@qq.com>
2024-08-29 17:27:37 +0800

    update to handle interaction in 2024-8-29

---
+++ Assets/_Assets/Scripts/GameInput.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour{
+    
+    private PlayerInputActions playerInputActions;
+    
+    private void Awake(){
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized(){
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+   
+        // 归一化向量 不会超过1
+        inputVector = inputVector.normalized;
+        return inputVector;
+    }
+}
--- Assets/_Assets/Scripts/Player.cs
+++ Assets/_Assets/Scripts/Player.cs
@@ -5,29 +5,63 @@ using UnityEngine;
 public class Player : MonoBehaviour{
     //SerializeField可以在unity编辑器使用
     [SerializeField] private float moveSpeed = 5f;
-    public float accelerate = 1.5f;
+    [SerializeField] private GameInput gameInput;
+    public float rotationSpeed = 10f;
+    private bool isWalking = false;
+
     void Update(){
-        Vector2 inputVector = new Vector2(0, 0);
-        float mos = moveSpeed;
-        if (Input.GetKey(KeyCode.W)){
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S)){
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A)){
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D)){
-            inputVector.x = +1;
-        }
-        if (Input.GetKey(KeyCode.LeftShift)){
-            mos = moveSpeed * accelerate;
-        }
-        // 归一化向量 不会超过1
-        inputVector = inputVector.normalized;
+        HandleMovement();
+        HandleInteraction();
+    }
+
+    private void HandleInteraction(){
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * Time.deltaTime * mos;
-        Debug.Log(inputVector);
+        float interactDistance = 2f;
+        if (Physics.Raycast(transform.position, moveDir, out RaycastHit raycastHit, interactDistance)){
+            Debug.Log(raycastHit.transform);
+        }else{
+            Debug.Log("-");   
+        }
+    }
+
+    private void HandleMovement(){
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+            playerRadius, moveDir, moveDistance);
+        if (!canMove){
+            //代表正在墙边
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                playerRadius, moveDirX, moveDistance);
+            if (canMove){
+                moveDir = moveDirX;
+            }
+            else{
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                    playerRadius, moveDirZ, moveDistance);
+                if (canMove){
+                    moveDir = moveDirZ;
+                }
+            }
+        }
+
+        if (canMove){
+            transform.position += moveDir * moveDistance;
+        }
+
+        isWalking = moveDir != Vector3.zero;
+        if (inputVector != Vector2.zero){
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+        }
+    }
+
+    public bool IsWalking(){
+        return isWalking;
     }
 }
