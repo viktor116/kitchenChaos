95b04758e9eaf18980f85c450ee49964efe023b3
soybean <984149293@qq.com>
2024-09-06 17:58:19 +0800

    update 9/6 6:44:39

--- Assets/_Assets/Scripts/PlateKitchenObject.cs
+++ Assets/_Assets/Scripts/PlateKitchenObject.cs
@@ -37,4 +37,9 @@ public class PlateKitchenObject : KitchenObject{
             return true;
         }
     }
+
+    public List<KitchenObjectSO> GetKitchenObjectSOList(){
+        return kitchenObjectSOList;
+    }
+    
 }
--- Assets/_Assets/Scripts/ScriptTableObjects/KitchenObjectSO.cs
+++ Assets/_Assets/Scripts/ScriptTableObjects/KitchenObjectSO.cs
@@ -3,10 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 [CreateAssetMenu()]
 public class KitchenObjectSO : ScriptableObject{
     public Transform prefab;
-    public Sprite Sprite;
+    [FormerlySerializedAs("Sprite")] public Sprite sprite;
     public string objectName;
 }
