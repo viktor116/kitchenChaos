257c7be4ee167ca5c4cb64634da61ed4664e047c
soybean <984149293@qq.com>
2024-09-10 17:46:50 +0800

    udpate 7:15

--- Assets/_Assets/Scripts/DeliveryManager.cs
+++ Assets/_Assets/Scripts/DeliveryManager.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class DeliveryManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+public class DeliveryManager : MonoBehaviour{
+
+    [SerializeField] private RecipeListSO recipeListSO;
+    
+    private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    private void Awake(){
+        waitingRecipeSOList = new List<RecipeSO>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update(){
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer <= 0f){
+            spawnRecipeTimer = spawnRecipeTimerMax;
+            if (waitingRecipeSOList.Count < waitingRecipesMax){
+                RecipeSO recipeSO = recipeListSO.recipeSOList[Random.Range(0,recipeListSO.recipeSOList.Count)];
+                Debug.Log(recipeSO.recipeName);
+                waitingRecipeSOList.Add(recipeSO);
+            }
+        }
     }
 }
