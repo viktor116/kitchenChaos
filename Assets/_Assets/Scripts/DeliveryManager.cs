cbeabdcd60f2f057c4f424921f2d18054a697ebd
98414 <984149293@qq.com>
2024-09-11 23:09:37 +0800

    update home 7:21

--- Assets/_Assets/Scripts/DeliveryManager.cs
+++ Assets/_Assets/Scripts/DeliveryManager.cs
@@ -7,6 +7,8 @@ using Random = UnityEngine.Random;
 
 public class DeliveryManager : MonoBehaviour{
 
+    public static DeliveryManager Instance{ get; private set; }
+
     [SerializeField] private RecipeListSO recipeListSO;
     
     private List<RecipeSO> waitingRecipeSOList;
@@ -15,6 +17,7 @@ public class DeliveryManager : MonoBehaviour{
     private int waitingRecipesMax = 4;
 
     private void Awake(){
+        Instance = this;
         waitingRecipeSOList = new List<RecipeSO>();
     }
 
@@ -28,5 +31,35 @@ public class DeliveryManager : MonoBehaviour{
                 waitingRecipeSOList.Add(recipeSO);
             }
         }
+    }
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject){
+        for (int i = 0; i < waitingRecipeSOList.Count; i++){
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+            if (waitingRecipeSO.KitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count){
+                bool plateContentsMatchesRecipe = true;
+                foreach (KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.KitchenObjectSOList){
+                    bool ingredientFount = false;
+                    foreach (KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList()){
+                        //盘子与配方的等待SO匹配
+                        if (plateKitchenObjectSO == recipeKitchenObjectSO){
+                            ingredientFount = true;
+                            break;
+                        }
+                    }
+                    if (!ingredientFount){
+                        plateContentsMatchesRecipe = false;
+                    }
+                }
+
+                if (plateContentsMatchesRecipe){
+                    //玩家配方正确   
+                    Debug.Log("player delivered the correct recipe");
+                    waitingRecipeSOList.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+        Debug.Log("player not delivered correct recipe error");
     }
 }
